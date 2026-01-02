05fd8deaa98f77e04eb2496f93e63737d7966bae
Sneha-datla <datla.sneha1@gmail.com>
2026-01-02 19:54:04 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ export default function App() {
           element={
             <>
              <HeroSection/>
-             <AboutSection/>
+             {/* <AboutSection/> */}
              <BlogSection/>
              <ResourcesSection/>
              <Contact/>
